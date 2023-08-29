d15d4d6f275a466b99d4b59a889518996b47853c
noreengithub <noreengul4@gmail.com>
2023-08-29 15:35:37 +0500

    error handling for http request

--- src/app/loading.interceptor.ts
+++ src/app/loading.interceptor.ts
@@ -3,12 +3,13 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { finalize } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError, finalize } from 'rxjs/operators';
 import { LoaderService } from './services/loader.service';
-
+import { DialogModalService } from './services/dialog-modal.service';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
@@ -16,7 +17,8 @@ export class LoadingInterceptor implements HttpInterceptor {
   private totalRequests = 0;
 
   constructor(
-    private loadingService: LoaderService
+    private loadingService: LoaderService,
+    private dialogModalService : DialogModalService
   ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
@@ -24,6 +26,21 @@ export class LoadingInterceptor implements HttpInterceptor {
     this.totalRequests++;
     this.loadingService.setLoading(true);
     return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse)=>{
+        let errorMsg = '';
+        console.log(errorMsg);
+        //alert("error");
+
+        this.dialogModalService.confirm('SomethingWrong','Something went wrong.','','', 'Ok','')
+    .then((confirmed) => {
+      if(confirmed){
+         
+      }
+    console.log('User confirmed:', confirmed)
+    })
+    .catch(() => console.log('User dismissed the dialog (e.g., by using ESC, clicking the cross icon, or clicking outside the dialog)'));
+        return throwError(errorMsg);
+      }),
       finalize(() => {
         this.totalRequests--;
         if (this.totalRequests == 0) {
